524a35836a01fb607092dee0f9ee782d3ccf6e90
Ferney Castañeda <dafcastanedag@correo.udistrital.edu.co>
2021-02-04 09:07:52 -0500

    Mailer: integrated

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -2,31 +2,68 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
+use App\Entity\Mailer;
+use App\Form\MailerType;
 use Symfony\Component\Mime\Email;
+use App\Repository\MailerRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/mailer")
+ */
 
 class MailerController extends AbstractController
 {
+
     /**
-     * @Route("/email")
+     * @Route("/", name="mailer_index", methods={"GET"})
      */
-    public function sendEmail(MailerInterface $mailer)
+    public function index(MailerRepository $mailerRepository): Response
     {
-        $email = (new Email())
+        return $this->render('mailer/index.html.twig', [
+            'mailers' => $mailerRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/new", name="mailer_new", methods={"GET","POST"})
+     */
+    public function new(MailerInterface $mailersym, Request $request): Response
+    {
+        $mailer = new Mailer();
+        $form = $this->createForm(MailerType::class, $mailer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($mailer);
+            $entityManager->flush();
+
+
+            $email = (new Email())
             ->from('admin.planestic@udistrital.edu.co')
-            ->to('ferneycastaneda@gmail.com.com')
+            ->to($mailer->getPara())
             //->cc('cc@example.com')
             //->bcc('bcc@example.com')
             //->replyTo('fabien@example.com')
             //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            // ->text('Sending emails is fun again!');
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+            ->subject($mailer->getAsunto())
+            ->text($mailer->getMensaje());
+            // ->html('<p>See Twig integration for better HTML integration!</p>');
 
-        $mailer->send($email);
+            $mailersym->send($email);
 
-        return $this->redirectToRoute('product_index');
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('mailer/new.html.twig', [
+            'mailer' => $mailer,
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Entity/Mailer.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MailerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MailerRepository::class)
+ */
+class Mailer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $para;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $asunto;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $mensaje;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPara(): ?string
+    {
+        return $this->para;
+    }
+
+    public function setPara(string $para): self
+    {
+        $this->para = $para;
+
+        return $this;
+    }
+
+    public function getAsunto(): ?string
+    {
+        return $this->asunto;
+    }
+
+    public function setAsunto(string $asunto): self
+    {
+        $this->asunto = $asunto;
+
+        return $this;
+    }
+
+    public function getMensaje(): ?string
+    {
+        return $this->mensaje;
+    }
+
+    public function setMensaje(string $mensaje): self
+    {
+        $this->mensaje = $mensaje;
+
+        return $this;
+    }
+}
